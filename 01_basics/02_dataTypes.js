b91b85db07c889d8fa51e73e625ecbacd822f51e
Abhinav290704 <144916084+Abhinav290704@users.noreply.github.com>
2025-09-28 13:42:38 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer JS version
+
+// alert(3+3) //we are using nodejs, not browser
+
+//primitive datatypes =>
+
+ let name = "Abhinav" //string
+ let age = 20 //number
+ let isLoggedIn = true //boolean
+
+ //null => standalone value, it is a representation of an empty value
+ //undefined => value assign nhi hui hai
+
+ //Non-primitive dataTypes =>
+ // Object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); // object
