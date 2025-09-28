0f4f9d8510bf55aaeb0ea43b828791e86abf5d0f
Abhinav290704 <144916084+Abhinav290704@users.noreply.github.com>
2025-09-28 14:47:16 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,10 @@ console.log(newNum); //290704
 console.log(typeof newNum); //string
 
 //We can easily convert a number into a string!
+
+/******************OPERATIONS****************/
+
+console.log("1" + 2); //Output => 12 because phle string dia hua hai toh string ki tarah he operation hoga
+console.log(1 + "2"); //12 because phle number dia hua hai toh number ki tarah he treat hoga and then number mai vo string add ho jaygi
+console.log("1" + 2 + 2);//122 same string hai first then next bhi string ki tarah add hoga
+console.log(1 + 2 + "2");//32 phle number hai toh number ki tarah operation perform hoga isliye phle 1 + 2 =3 then 3 mai vo string add hogi
