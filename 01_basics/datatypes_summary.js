681094a8ee85d07d008d712496806b795dff527e
Abhinav290704 <144916084+Abhinav290704@users.noreply.github.com>
2025-09-30 12:17:52 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,25 @@
+/* Data memory mai kis tarah se store hota hai
+aur kis tarah se access hota hai, isi ke base pr datatypes are divided into two categories
+primitive datatypes and
+non-primitive datatyes*/
+
+/* # Primitive :=>
+ These are call by value, original data nii dia jata hai, ek copy di jati hai data ki
+there is no changes in original data
+7 types: String, Number, Boolean, null, undefined, symbol, BigInt  
+
+  # Non-Primitive Datatype :=>
+    Array,
+        let heros = ["shaktiman", "naagraj", "doga"];
+  Objects,
+        let myObj = {
+        name: "Abhinav",
+        age: 22,
+        }
+  Functions
+        let myFunction = function(){
+        console.log("Hello world");
+        }
+
+*/
+
